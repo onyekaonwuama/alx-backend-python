8630396fdf16a1e0e77f6a640b2b16d023759e18
Onyeka <nnyeka212@gmail.com>
2025-07-07 21:42:57 +0200

    Added log queries

--- python-decorators-0x01/0-log_queries.py
+++ python-decorators-0x01/0-log_queries.py
@@ -1,14 +1,25 @@
 import sqlite3
 import functools
 
-#### decorator to lof SQL queries
-
-""" YOUR CODE GOES HERE"""
 def log_queries(func):
+    """Decorator to log SQL queries before executing them"""
     @functools.wraps(func)
-    def wrapper(query):
-        print(query)
-        return func(query)
+    def wrapper(*args, **kwargs):
+        # Extract the query from the function arguments
+        # Assuming the query is passed as a keyword argument or first positional argument
+        query = None
+        if 'query' in kwargs:
+            query = kwargs['query']
+        elif args and isinstance(args[0], str):
+            query = args[0]
+        
+        # Log the query if found
+        if query:
+            print(f"Executing query: {query}")
+        
+        # Execute the original function
+        return func(*args, **kwargs)
+    
     return wrapper
 
 @log_queries
@@ -20,5 +31,5 @@ def fetch_all_users(query):
     conn.close()
     return results
 
-#### fetch users while logging the query
+# Fetch users while logging the query
 users = fetch_all_users(query="SELECT * FROM users")
