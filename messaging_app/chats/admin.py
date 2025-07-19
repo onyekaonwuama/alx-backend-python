1813f7c94413aa908da869622a8a5c01153028e5
Onyeka <nnyeka212@gmail.com>
2025-07-19 19:35:10 +0200

    Ran the tests

--- messaging_app/chats/admin.py
+++ messaging_app/chats/admin.py
@@ -1,6 +1,46 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from .models import User, Conversation, Message
 
-admin.site.register(User)
-admin.site.register(Conversation)
-admin.site.register(Message)
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    list_display = ('email', 'first_name', 'last_name', 'role', 'created_at')
+    list_filter = ('role', 'created_at')
+    search_fields = ('email', 'first_name', 'last_name')
+    ordering = ('email',)
+    
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'phone_number')}),
+        ('Permissions', {'fields': ('role', 'is_active', 'is_staff', 'is_superuser')}),
+        ('Important dates', {'fields': ('last_login', 'created_at')}),
+    )
+    
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'first_name', 'last_name', 'password1', 'password2', 'role'),
+        }),
+    )
+
+@admin.register(Conversation)
+class ConversationAdmin(admin.ModelAdmin):
+    list_display = ('conversation_id', 'created_at', 'participant_count')
+    list_filter = ('created_at',)
+    search_fields = ('participants__email', 'participants__first_name', 'participants__last_name')
+    filter_horizontal = ('participants',)
+    
+    def participant_count(self, obj):
+        return obj.participants.count()
+    participant_count.short_description = 'Participants'
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('message_id', 'sender', 'conversation', 'sent_at', 'message_preview')
+    list_filter = ('sent_at',)
+    search_fields = ('sender__email', 'message_body')
+    readonly_fields = ('message_id', 'sent_at')
+    
+    def message_preview(self, obj):
+        return obj.message_body[:50] + "..." if len(obj.message_body) > 50 else obj.message_body
+    message_preview.short_description = 'Message Preview'
--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -1,7 +1,48 @@
 import uuid
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
 from django.utils import timezone
+
+class UserManager(BaseUserManager):
+    """
+    Custom user manager that uses email instead of username
+    """
+    def create_user(self, email, first_name, last_name, password=None, **extra_fields):
+        """
+        Create and save a regular user with the given email and password
+        """
+        if not email:
+            raise ValueError('The Email field must be set')
+        if not first_name:
+            raise ValueError('The First Name field must be set')
+        if not last_name:
+            raise ValueError('The Last Name field must be set')
+            
+        email = self.normalize_email(email)
+        user = self.model(
+            email=email,
+            first_name=first_name,
+            last_name=last_name,
+            **extra_fields
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, first_name, last_name, password=None, **extra_fields):
+        """
+        Create and save a superuser with the given email and password
+        """
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('role', 'admin')
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self.create_user(email, first_name, last_name, password, **extra_fields)
 
 class User(AbstractUser):
     """
@@ -35,6 +76,9 @@ class User(AbstractUser):
     username = None
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['first_name', 'last_name']
+    
+    # Use the custom manager
+    objects = UserManager()
     
     class Meta:
         db_table = 'users'
